76208ca0e50b93973c96559538605cce8bcdcf8b
reisryan <211036132@aluno.unb.br>
2024-12-02 16:15:04 -0300

    teste 0 - primeira falha

---
+++ README.md
@@ -0,0 +1,5 @@
+# Trabalho de Métodos de Programação
+
+## Aluno: Ryan Reis Fontenele
+## Matrícula: 211036132
+## Semestre 2024.2
---
+++ velha.cpp
@@ -0,0 +1,19 @@
+/**
+ * \file  velha.cpp
+ */
+
+ 
+#include "velha.hpp"
+
+/** 
+ * @brief verifica situacao do jogo da velha  
+ * @author Ryan Reis
+ * @param  velha - não faz nada;
+ */ 
+
+int VerificaVelha( int velha[3][3] )
+{
+	return 0; /*!< retorna zero para teste */ 
+}
+
+
