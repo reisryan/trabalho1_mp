06d548714319e390cacc6cd64b6bcc6dd5e0db1f
reisryan <211036132@aluno.unb.br>
2024-12-02 17:07:41 -0300

    teste 2 - segunda falha

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -8,11 +8,12 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-TEST_CASE( "Testa velha", "[single-file]" ) {
-    int teste1[3][3]= {   { 2, 0, 1 }, 
-	                      { 2, 0, 1 },
-						  { 0, 2, 1 }
-						  };
+TEST_CASE("Testa velha", "[single-file]") {
+    int teste1[3][3]= {{2,0,1},{2,0,1},{0,2,1}};
     REQUIRE(VerificaVelha(teste1)== 1);
 } 
+TEST_CASE("Testa -1", "[single-file]") {
+	int teste2[3][3]={{1,1,1},{1,1,1},{1,1,1}};
+	REQUIRE(VerificaVelha(teste2)== -2);
+}
  
