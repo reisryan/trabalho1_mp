5a56bcd9fa52d8056724b4864981ed69b1b3090a
reisryan <211036132@aluno.unb.br>
2024-12-03 12:29:10 -0300

    teste 10 - melhorias

--- velha.cpp
+++ velha.cpp
@@ -8,6 +8,34 @@
  * @param  velha - verifica se X venceu, ou se é impossível pelas regras;
  */ 
 
+int VerificaVitoria(int velha[3][3]) {
+    /* Verifica se há vitória em linhas j */
+    for (int j = 0; j <= 2; j++) {
+        if ((velha[j][0] == velha[j][1] && velha[j][1] == velha[j][2]) &&
+        (velha[j][0] == X || velha[j][0] == O)) {
+            return velha[j][0];
+        }
+    }
+    /* Verifica se há vitória em colunas i */
+    for (int i = 0; i <= 2; i++) {
+        if ((velha[0][i] == velha[1][i] && velha[1][i] == velha[2][i]) &&
+        (velha[0][i] == X || velha[0][i] == O)) {
+            return velha[0][i];
+        }
+    }
+    /* Verifica vitória na diagonal principal */
+    if ((velha[0][0] == velha[1][1] && velha[1][1] == velha[2][2]) &&
+    (velha[0][0] == X || velha[0][0] == O)) {
+        return velha[0][0];
+    }
+    /* Verifica vitória na diagonal secundária */
+    if ((velha[0][2] == velha[1][1] && velha[1][1] == velha[2][0]) &&
+    (velha[1][1] == X || velha[1][1] == O)) {
+        return velha[1][1];
+    }
+
+    return 0;
+}
 bool VerificaRegras(int velha[3][3]) {
     int primeiro = velha[0][0];
     if (primeiro != 1 && primeiro != 2) {
@@ -30,10 +58,10 @@ bool VerificaEmpate(int velha[3][3]) {
 
     for (int i=0; i <= 2; i++) {
         for (int j=0; j <= 2; j++) {
-            if (velha[i][j] == 1) {
+            if (velha[i][j] == X) {
                 quantX += 1;
             }
-            if (velha[i][j] == 2) {
+            if (velha[i][j] == O) {
                 quantO += 1;
             }
         }
@@ -50,9 +78,9 @@ bool VerificaIndefinido(int velha[3][3]) {
     // Conta os elementos no tabuleiro
     for (int i = 0; i <= 2; i++) {
         for (int j = 0; j <= 2; j++) {
-            if (velha[i][j] == 1) {
+            if (velha[i][j] == X) {
                 quantX += 1;
-            } else if (velha[i][j] == 2) {
+            } else if (velha[i][j] == O) {
                 quantO += 1;
             }
         }
@@ -60,7 +88,9 @@ bool VerificaIndefinido(int velha[3][3]) {
     if ((quantX > 3 || quantO > 3) && (VerificaRegras(velha) == false)) {
         return true;
     }
-    if (((quantX == 0 || quantO == 0) || (quantX < 3 && quantO < 3 && quantX != quantO)) && (quantO != 9 && quantX != 9)) {
+    if (((quantX == 0 || quantO == 0) ||
+    (quantX < 3 && quantO < 3 && quantX != quantO)) &&
+    (quantO != 9 && quantX != 9)) {
         return true;
     }
 
@@ -80,29 +110,8 @@ int VerificaVelha(int velha[3][3]) {
     if (VerificaRegras(velha)) {
         return -2;
     }
-    /* Verifica se há vitória em linhas j */
-    for (int j = 0; j <= 2; j++) {
-        if ((velha[j][0] == velha[j][1] && velha[j][1] == velha[j][2]) &&
-        (velha[j][0] == 1 || velha[j][0] == 2)) {
-            return velha[j][0];
-        }
-    }
-    /* Verifica se há vitória em colunas i */
-    for (int i = 0; i <= 2; i++) {
-        if ((velha[0][i] == velha[1][i] && velha[1][i] == velha[2][i]) &&
-        (velha[0][i] == 1 || velha[0][i] == 2)) {
-            return velha[0][i];
-        }
-    }
-    /* Verifica vitória na diagonal principal */
-    if ((velha[0][0] == velha[1][1] && velha[1][1] == velha[2][2]) &&
-    (velha[0][0] == 1 || velha[0][0] == 2)) {
-        return velha[0][0];
-    }
-    /* Verifica vitória na diagonal secundária */
-    if ((velha[0][2] == velha[1][1] && velha[1][1] == velha[2][0]) &&
-    (velha[1][1] == 1 || velha[1][1] == 2)) {
-        return velha[1][1];
+    if (VerificaVitoria(velha) == 1 || VerificaVitoria(velha) == 2) {
+        return VerificaVitoria(velha);
     }
 
     /* Caso nenhuma das condições anteriores seja satisfeita */
--- velha.hpp
+++ velha.hpp
@@ -1,11 +1,12 @@
 /* Copyright [2024] <Copyright Ryan> */
-#ifndef VELHA_HPP
-#define VELHA_HPP
+#ifndef VELHA_HPP_
+#define VELHA_HPP_
 constexpr int X = 1;
 constexpr int O = 2;
 #include <cmath>
 int VerificaVelha(int velha[3][3]);
+int VerificaVitoria(int velha[3][3]);
 bool VerificaRegras(int velha[3][3]);
 bool VerificaEmpate(int velha[3][3]);
 bool VerificaIndefinido(int velha[3][3]);
-#endif
+#endif  //  VELHA_HPP_
